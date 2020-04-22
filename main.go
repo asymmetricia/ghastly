67a8a0d5a7bbc652651db89bd66b388ce664ec83
tricia <tricia-github@cernu.us>
2020-04-21 23:57:44 -0700

    fix main, heh

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
+import (
+	"fmt"
+	"github.com/pdbogen/ghastly/cmd"
+	"os"
+)
+
 func main() {
-	if err := rootCmd.Execute(); err != nil {
+	if err := cmd.Root.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
