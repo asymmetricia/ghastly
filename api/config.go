f72c7b9fd233297f254dfb16fdd95e51d077c0c1
tricia <tricia-github@cernu.us>
2020-05-01 01:19:03 -0700

    support some more config-oriented endpoints

--- api/config.go
+++ api/config.go
@@ -1,7 +1,9 @@
 package api
 
 import (
-	"errors"
+	"bytes"
+	"encoding/json"
+	"fmt"
 )
 
 // https://github.com/home-assistant/core/blob/master/homeassistant/components/config/config_entries.py#L79
@@ -53,9 +55,108 @@ func (c *Client) ListConfigEntries() ([]*ConfigEntry, error) {
 	return ret.([]*ConfigEntry), nil
 }
 
-type ConfigFlow struct{}
+type Config struct {
+	Components            []string          `json:"components"`
+	ConfigDir             string            `json:"config_dir"`
+	ConfigSource          string            `json:"config_source"`
+	Elevation             int               `json:"elevation"`
+	Latitude              float64           `json:"latitude"`
+	LocationName          string            `json:"location_name"`
+	Longitude             float64           `json:"longitude"`
+	TimeZone              string            `json:"time_zone"`
+	UnitSystem            map[string]string `json:"unit_system"`
+	Version               string            `json:"version"`
+	WhitelistExternalDirs []string          `json:"whitelist_external_dirs"`
+}
 
-// ListConfigFlows would list known config flows, but the requisite API endpoint is not implemented.
-func (c *Client) ListConfigFlows() ([]*ConfigFlow, error) {
-	return nil, errors.New("unimplemented")
+type GetConfigMessage struct{}
+
+func (GetConfigMessage) Type() string { return "get_config" }
+
+func (c *Client) GetConfig() (*Config, error) {
+	retI, err := c.RawWebsocketRequestAs(GetConfigMessage{}, &Config{})
+	if err != nil {
+		return nil, err
+	}
+
+	ret, ok := retI.(*Config)
+	if !ok {
+		return nil, fmt.Errorf("returned object was %T, not *Config", retI)
+	}
+
+	return ret, nil
+}
+
+type ConfigFlowProgress struct {
+	Context ConfigFlowProgressContext `json:"context"`
+	FlowId  ConfigFlowId              `json:"flow_id"`
+	Handler string                    `json:"handler"`
+}
+
+type ConfigFlowProgressContext struct {
+	Source string `json:"source"`
+}
+
+type ConfigFlowId string
+
+type ListConfigFlowProgressMessage struct{}
+
+func (ListConfigFlowProgressMessage) Type() string { return "config_entries/flow/progress" }
+
+func (c *Client) ListConfigFlowProgress() ([]ConfigFlowProgress, error) {
+	retI, err := c.RawWebsocketRequestAs(ListConfigFlowProgressMessage{}, []ConfigFlowProgress{})
+	if err != nil {
+		return nil, err
+	}
+
+	ret, ok := retI.([]ConfigFlowProgress)
+	if !ok {
+		return nil, fmt.Errorf("returned object was %T, not []ConfigFlowProgress", retI)
+	}
+
+	return ret, nil
+}
+
+type ConfigFlow struct {
+	DataSchema              []ConfigFlowDataSchema `json:"data_schema"`
+	DescriptionPlaceholders interface{}            `json:"description_placeholders"`
+	Errors                  map[string]interface{} `json:"errors"`
+	FlowId                  ConfigFlowId           `json:"flow_id"`
+	Handler                 string                 `json:"handler"`
+	StepId                  string                 `json:"step_id"`
+	Type                    string                 `json:"type"`
+}
+
+type ConfigFlowDataSchema struct {
+	Name     string `json:"name"`
+	Required bool   `json:"required"`
+	Type     string `json:"type"`
+}
+
+func (c *Client) GetFlow(id ConfigFlowId) (*ConfigFlow, error) {
+	retI, err := c.RawRESTRequestAs("config/config_entries/flow/"+string(id), nil, (*ConfigFlow)(nil))
+	if err != nil {
+		return nil, err
+	}
+
+	ret, ok := retI.(*ConfigFlow)
+	if !ok {
+		return nil, fmt.Errorf("returned object was %T, not *ConfigFlow")
+	}
+
+	return ret, nil
+}
+
+func (c *Client) SetFlow(id ConfigFlowId, payload map[string]interface{}) error {
+	jsonPayload, err := json.Marshal(payload)
+	if err != nil {
+		return fmt.Errorf("could not marshal payload to JSON: %v", err)
+	}
+
+	_, err = process(c.Post("config/config_entries/flow/"+string(id), bytes.NewBuffer(jsonPayload)))
+	if err != nil {
+		return fmt.Errorf("POSTing set-flow: %v", err)
+	}
+
+	return nil
 }
